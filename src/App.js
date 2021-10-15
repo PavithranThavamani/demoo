71a90dd83d5d699f591a1322e9dde9fb95cc87ab
PavithranThavamani <pavithranthavamani708@gmail.com>
2021-10-15 20:21:41 +0530

    check1

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save t00000o reload.
+          Edit <code>src/App.js</code> and save t00000000o reload.
         </p>
         <a
           className="App-link"
